893d1f746da22f7d990f404be2dffe3f6e0db3d9
charmude10 <abioyemichael4@gmail.com>
2023-12-05 08:36:13 -0800

    data_structure

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,10 +3,10 @@ def print_matrix_integer(matrix=[[]]):
     if not matrix:
         print()
     else:
-        for item in range(len(matrix[row]):
-                for item in range(len(matrix[row]) - 1:
-                                  endspace = ' '
-                                  else:
-                                  endspace = ' '
-                                  print("{d}".format(matrix[row][item]), end=enspace)
-                                  print()
+        for row in range(len(matrix)):
+            for item in range(len(matrix[row]) - 1:
+                    endspace = ' '
+                    else:
+                    endspace = ''
+                    print("{:d}".format(matrix[row][item]), end=endspace)
+                    print()
