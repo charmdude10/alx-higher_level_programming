2b467d67bf215a99a207ef2094b552edf1ce52d6
charmude10 <abioyemichael4@gmail.com>
2023-12-04 18:08:08 -0800

    my python data structures

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        print()
+    else:
+        for item in range(len(matrix[row]):
+                for item in range(len(matrix[row]) - 1:
+                                  endspace = ' '
+                                  else:
+                                  endspace = ' '
+                                  print("{d}".format(matrix[row][item]), end=enspace)
+                                  print()
---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_return(senrence):
+    if not sentense:
+        sentense = None
+        if senence:
+            sen_len = 0
+            return (sen_len, sentence if not sentence else sentence[:1])
